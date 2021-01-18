5bed2297310da6a251248a234cb14a2b4bf7a697
George Khankeldian <georgekhankeldian@gmail.com>
2021-01-17 22:22:10 -0600

    begin testing displaying pokeapi data on webpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var Pokedex = require('pokedex-promise-v2');
+var React = require('react');
+var P = new Pokedex();
+var Eevee = 1;
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      EeveeName: "",
+    } 
+   }
+
+  componentDidMount() {
+    var self = this;
+    P.getPokemonByName(2) // with Promise
+    .then(function(response) {
+
+
+      self.setState({
+        EeveeName: response.species.name,
+      })
+
+      //Eevee = response;
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log('There was an ERROR: ', error);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 id = "foo">
+            {this.state.EeveeName}
+          </h1>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
